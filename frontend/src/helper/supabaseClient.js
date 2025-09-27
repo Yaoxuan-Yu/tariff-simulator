344ce88634ecd3a898d842a78ffd4e44dccbc4cc
kirthivasshni <kirthiv.l.2023@scis.smu.edu.sg>
2025-09-28 00:42:03 +0800

    merged frontend + linked with supabase auth

--- frontend/src/helper/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://eommtatuhdmnkakktghz.supabase.co';
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvbW10YXR1aGRtbmtha2t0Z2h6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODI5NjEzMCwiZXhwIjoyMDczODcyMTMwfQ.c9xhPA5Hfm9aXdfPhn20mzdlNcH3olXaiJ5o8EoydS8";
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-export default supabase;
